cf79c2437cb0429b599aa540f33724069c6c4786
Alok <70504068+aj1412@users.noreply.github.com>
2021-06-30 10:36:54 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -7,7 +7,8 @@ import * as dat from 'dat.gui'
 
 const textureloader = new THREE.TextureLoader()
 
-const normaltexture = textureloader.load('/texture/NomrlaMap.png')
+const normaltexture = textureloader.load('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ91r75BDKONf7u1pKZ7wBNhtpkgeiMt9aOvg&usqp=CAU')
+const layer = textureloader.load('https://static3.depositphotos.com/1000887/121/i/950/depositphotos_1215336-stock-photo-beige-rough-stone-wall-texture.jpg')
 
 // Debug
 const gui = new dat.GUI()
@@ -23,11 +24,25 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 
 // Materials
 
-const material = new THREE.MeshStandardMaterial()
-material.metalness = 0.7
-material.roughness = 0.2
-material.normalMap = normaltexture
+const material = new THREE.MeshStandardMaterial(0x49ef4)
+material.material = 1
+material.roughness = 0.46
+material.metalness = 0.64
+material.Map = normaltexture
+material.normalMap = layer
 material.color = new THREE.Color(0x292929)
+
+const meshmaterial = gui.addFolder('material')
+
+meshmaterial.add(material, 'material').min(0).max(1).step(0.01)
+meshmaterial.add(material, 'roughness').min(0).max(1).step(0.01)
+meshmaterial.add(material, 'metalness').min(0).max(1).step(0.01)
+meshmaterial.add(material, 'wireframe')
+meshmaterial.add(material, 'flatShading')
+meshmaterial.add(material, 'vertexColors')
+// meshmaterial.add(material, 'emissive')
+
+
 
 // Mesh
 const sphere = new THREE.Mesh(geometry, material)
@@ -35,15 +50,34 @@ scene.add(sphere)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+const pointLight = new THREE.PointLight(0xffffff)
+const ambient_light = new THREE.AmbientLight( 0xffffff );
+pointLight.position.x = 5
+pointLight.position.y = 0.54
+pointLight.position.z = -0.08
+pointLight.intensity = 10
+scene.add(pointLight , ambient_light)
+
+const light1 = gui.addFolder('Light1')
+
+light1.add(pointLight.position, 'x').min(-3).max(6).step(0.01)
+light1.add(pointLight.position, 'y').min(-6).max(6).step(0.01)
+light1.add(pointLight.position, 'z').min(-3).max(6).step(0.01)
+light1.add(pointLight, 'intensity').min(0).max(6).step(0.01)
+
+const lightColor = {
+  color: 0xff000,
+}
+light1.addColor(lightColor, 'color').onChange(() => {
+  pointLight.color.set(light.color)
+})
+
+// const pointLightHelper = new THREE.PointLightHelper(pointLight, 1)
+// scene.add(pointLightHelper)
 
 // Light 2
-const pointLight2 = new THREE.PointLight(0xff0000, 2)
-pointLight2.position.set(-1.32, -0.82, 1.45)
+const pointLight2 = new THREE.PointLight(0xffffff)
+pointLight2.position.set(-2.31, -0.26, 0.2)
 pointLight2.intensity = 10
 
 scene.add(pointLight2)
@@ -55,15 +89,16 @@ light.add(pointLight2.position, 'y').min(-6).max(6).step(0.01)
 light.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
 light.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
 
-const lightColor = {
+const lightColor1 = {
   color: 0xff000,
 }
-light.addColor(lightColor, 'color').onChange(() => {
+light.addColor(lightColor1, 'color').onChange(() => {
   pointLight2.color.set(light.color)
 })
 
-// const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1)
-// scene.add(pointLightHelper)
+// const pointLightHelper1 = new THREE.PointLightHelper(pointLight2, 1)
+// scene.add(pointLightHelper1)
+
 
 /**
  * Sizes
@@ -102,6 +137,7 @@ camera.position.y = 0
 camera.position.z = 2
 scene.add(camera)
 
+
 // Controls
 // const controls = new OrbitControls(camera, canvas)
 // controls.enableDamping = true
@@ -116,40 +152,56 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+
+// Camera Rotation Control
+const controls = new OrbitControls( camera, renderer.domElement );
+
+controls.rotateSpeed = 1;
+controls.zoomSpeed = 0.9;
+
+controls.minDistance = 3;
+controls.maxDistance = 200;
+
+controls.minPolarAngle = 0; // radians
+controls.maxPolarAngle = Math.PI /2; // radians
+
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+
 /**
  * Animate
  */
 
-document.addEventListener('mousemove', onDocumentMouseMove)
+// document.addEventListener('mousemove', onDocumentMouseMove)
 
-let mouseX = 0
-let mouseY = 0
+// let mouseX = 0
+// let mouseY = 0
 
-let targetx = 0
-let targety = 0
+// let targetx = 0
+// let targety = 0
 
-const windowX = window.innerWidth / 2
-const windowY = window.innerHeight / 2
+// const windowX = window.innerWidth / 2
+// const windowY = window.innerHeight / 2
 
-function onDocumentMouseMove(event) {
-  mouseX = event.clientX - windowX
-  mouseY = event.clientY - windowY
-}
+// function onDocumentMouseMove(event) {
+//   mouseX = event.clientX - windowX
+//   mouseY = event.clientY - windowY
+// }
 
 const clock = new THREE.Clock()
 
 const tick = () => {
-  targetx = mouseX * 0.001
-  targety = mouseY * 0.001
+  // targetx = mouseX * 0.001
+  // targety = mouseY * 0.001
 
   const elapsedTime = clock.getElapsedTime()
 
   // Update objects
-  sphere.rotation.y = 0.5 * elapsedTime
+  // sphere.rotation.y = 0.5 * elapsedTime
 
-  sphere.rotation.y += 0.5 * (targetx - sphere.rotation.y)
-  sphere.rotation.x += 0.5 * (targetx - sphere.rotation.x)
-  sphere.rotation.z += 0.5 * (targetx - sphere.rotation.x)
+  // sphere.rotation.y += 0.5 * (targetx - sphere.rotation.y)
+  // sphere.rotation.x += 0.5 * (targetx - sphere.rotation.x)
+  // sphere.rotation.z += 0.5 * (targetx - sphere.rotation.x)
 
   sphere.rotat
 
